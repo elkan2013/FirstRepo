6824b95ec2b70be6b539c845c2eb615a565a7b2a
elkan <jee_qi_he_elkan@s2026.ssts.edu.sg>
2026-08-31 16:10:59 +0800

    Initial Commit

---
+++ FirstRepo/FirstRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstRepoApp.swift
+//  FirstRepo
+//
+//  Created by Elkan Jee on 31/8/26.
+//
+
+import SwiftUI
+
+@main
+struct FirstRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
